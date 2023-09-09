0300248d82e39f1d9f2554dcd76d8b48dbd7b508
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-09-10 02:19:18 +0400

    update

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -3,7 +3,10 @@ package school.mjc.stage0.loops.finalTask;
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
         for (int i = 1; i <= cathetusLength; i++) {
-            for (int j = 1; j <= i; j++) {
+            for (int j = cathetusLength; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -14,4 +17,5 @@ public class HalfPyramid {
         new HalfPyramid().printHalfPyramid(8);
     }
 }
+
 
