d685c3d4ec59047c282c42a6fc11fd291affcf3f
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-09-10 02:16:34 +0400

    update

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,12 +3,21 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
         for (int i = 1; i <= cathetusLength; i++) {
+            // Print spaces
             for (int j = 1; j <= cathetusLength - i; j++) {
                 System.out.print(" ");
             }
-            for (int k = 1; k <= i * 2 - 1; k++) {
-                System.out.print("*");
+
+            // Print decreasing numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
             }
+
+            // Print increasing numbers
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+
             System.out.println();
         }
     }
